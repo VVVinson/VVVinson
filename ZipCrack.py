447100a82bc82b8eb0f9dedf7543b7150c3098d9
VVVinson <459789875@qq.com>
2017-09-22 20:47:24 +0800

    Add files via upload

---
+++ ZipCrack.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import zipfile
+import optparse
+from threading import Thread
+
+
+def extractFile(zFile, password):
+    try:
+        zFile.extractall(pwd = password)
+        print '[+] Found password ' + password + '\n'
+    except:
+        pass
+
+def main():
+    parser = optparse.OptionParser("usage%prog "+ "-f <zipfile> -d <dictionary>")
+    parser.add_option('-f', '--file', dest='zname', type='string', help='The zip file which you want to crack')
+    parser.add_option('-d', '--dictionary', dest='dname', type='string', help='The password dictionary')
+    (options, args) = parser.parse_args()
+    if (options.zname == None) | (options.dname == None):
+        print parser.usage
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+
+    zFile = zipfile.ZipFile(zname)
+    passFile = open(dname)
+
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        t = Thread(target = extractFile, args =(zFile, password))
+        t.start()
+
+if __name__ == '__main__':
+    main()
